47ce9e095d5fc9c94aee34a031abc4b1290f133a
許慈忻 <evahsu1101@g.ncu.edu.tw>
2024-11-17 16:39:58 +0800

    Update CWGANGP.py

--- CWGANGP.py
+++ CWGANGP.py
@@ -98,7 +98,7 @@ model_save_dir = r"./model"
 custom_dataset = CustomDataset(root_folder, transform=transform)
 
 # dataloader
-batch_size = 43
+batch_size = 6
 data_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
 
 
